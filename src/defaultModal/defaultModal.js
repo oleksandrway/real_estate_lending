fb58a28e7e9b536c1ddab865b6185d3e760e308d
Oleksandr Kryvoruchko <oleksandrkryvoruchko2000@gmail.com>
2022-09-21 17:53:40 +0300

    changed defaultModal
    now it doesn't make copy of content
    and don't create every time and only hide and show modal

--- src/defaultModal/defaultModal.js
+++ src/defaultModal/defaultModal.js
@@ -1,30 +1,30 @@
 
 import './defaultModal.scss'
 
-function createModal({ preContent, closeElementsClasses }) {
+function createModal({ preContentClass, closeElementsClasses }) {
+  const preContent = document.querySelector(preContentClass)
   let content
   if (!preContent || typeof preContent === 'string') {
-    console.warn('you should pass an element in function openModal')
+    console.warn('you should pass a class of  element in function openModal')
     content = document.createElement('div')
     content.classList.add('error')
     content.innerText = 'not found'
   }
   else {
-    content = preContent.cloneNode(true)
-
+    content = preContent
     content.removeAttribute('hidden')
   }
 
   const defaultOverlay = document.createElement('div')
   defaultOverlay.className = 'defaultOverlay defaultOverlay--active'
 
-  function removeModal() {
-    defaultOverlay.remove()
+  function hideModal() {
+    defaultOverlay.classList.remove('defaultOverlay--active')
     document.body.classList.remove('no-scroll')
   }
 
   const defaultModal = document.createElement('div')
-  defaultModal.className = 'defaultModal defaultModal--active'
+  defaultModal.className = 'defaultModal'
 
   const modalClose = document.createElement('div')
 
@@ -39,31 +39,38 @@ function createModal({ preContent, closeElementsClasses }) {
   defaultModal.append(modalClose)
   defaultModal.append(content)
 
-  modalClose.addEventListener('click', removeModal)
+  modalClose.addEventListener('click', hideModal)
 
   defaultOverlay.addEventListener('click', (e) => {
     if (e.target === defaultOverlay)
-      removeModal()
+      hideModal()
   })
   document.addEventListener('keydown', (e) => {
     if (e.code === 'Escape')
-      removeModal()
+      hideModal()
   })
 
   if (closeElementsClasses.length) {
     closeElementsClasses.forEach((closeElementClass) => {
       const closeElement = defaultOverlay.querySelector(closeElementClass)
-      closeElement.addEventListener('click', removeModal)
+      if (closeElement)
+        closeElement.addEventListener('click', hideModal)
     })
   }
 
   return defaultOverlay
 }
 
-function openModal({ preContent, closeElementsClasses }) {
-  const defaultModal = createModal({ preContent, closeElementsClasses })
-
-  document.body.appendChild(defaultModal)
+function openModal({ preContentClass, closeElementsClasses }) {
+  const contentInModal = document.querySelector(`.defaultOverlay ${preContentClass}`)
+  if (contentInModal) {
+    const modal = contentInModal.closest('.defaultOverlay')
+    modal.classList.add('defaultOverlay--active')
+  }
+  else {
+    const modal = createModal({ preContentClass, closeElementsClasses })
+    document.body.appendChild(modal)
+  }
 
   document.body.classList.add('no-scroll')
 }
--- src/js/components/favorite-modal.js
+++ src/js/components/favorite-modal.js
@@ -1,8 +1,8 @@
 import { openModal } from '@/defaultModal/defaultModal.js'
 
 const favoriteModalBtn = document.querySelector('#favorite-modal-btn')
-const favoriteModal = document.querySelector('.favorite-modal')
+// const favoriteModal = document.querySelector('.favorite-modal')
 
 favoriteModalBtn.addEventListener('click', () => {
-  openModal({ preContent: favoriteModal, closeElementsClasses: ['.order-call__btn'] })
+  openModal({ preContentClass: '.favorite-modal', closeElementsClasses: ['.order-call__btn'] })
 })
